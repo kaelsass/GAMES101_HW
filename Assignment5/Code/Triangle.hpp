d7767bc8b8b4fd4e0162544f454fcd66652ed838
sima <sima@bilibili.com>
2022-01-30 11:42:17 +0800

    初步实现射线生成、三角形相交判断逻辑

--- Assignment5/Code/Triangle.hpp
+++ Assignment5/Code/Triangle.hpp
@@ -11,6 +11,21 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    Vector3f E1 = v1 - v0;
+    Vector3f E2 = v2 - v0;
+    Vector3f S = orig - v0;
+    Vector3f S1 = crossProduct(dir, E2);
+    Vector3f S2 = crossProduct(S, E1);
+    float scale = dotProduct(S1, E1);
+    float t = scale * dotProduct(S2, E2);
+    float b1 = scale * dotProduct(S1, S);
+    float b2 = scale * dotProduct(S2, dir);
+    if (t >= 0 && b1 >= 0 && b2 >= 0 && (b1 + b2) <= 1) {
+        tnear = t;
+        u = b1;
+        v = b2;
+        return true;
+    }
     return false;
 }
 
